8f50d9e43671d13800d2f4a69f8e2bc33910aff5
Alan Evans <thealanevans@googlemail.com>
2016-02-27 13:36:39 +0000

    Some initial tests with one item

---
+++ bin-pack/PackResult.cs
@@ -0,0 +1,9 @@
+﻿namespace Mercury.BinPack
+{
+    public sealed class PackResult
+    {
+        public int TotalWidth { get; set; }
+
+        public int TotalHeight { get; set; }
+    }
+}
---
+++ bin-pack/Packer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Mercury.BinPack
+{
+    public sealed class Packer<T>
+    {
+        private readonly Func<T,int> _getWidth;
+        private readonly Func<T, int> _getHeight;
+
+        public Packer(Func<T,int> getWidth, Func<T,int> getHeight)
+        {
+            _getWidth = getWidth;
+            _getHeight = getHeight;
+        }
+
+        public PackResult Pack(T item)
+        {
+            return new PackResult { 
+                TotalWidth = _getWidth(item),
+                TotalHeight = _getHeight(item)
+            };
+        }
+    }
+}
