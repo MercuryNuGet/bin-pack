142d10dd2602e87381de87e4d2fc0663cc057ca2
Alan Evans <alan.evans@burrows.yr.com>
2016-03-01 09:03:24 +0000

    Failing test

--- bin-pack-tests/PackingCases.cs
+++ bin-pack-tests/PackingCases.cs
@@ -15,6 +15,11 @@ namespace Mercury.BinPack.Tests
 
             public int Width { get; set; }
             public int Height { get; set; }
+
+            public override string ToString()
+            {
+                return $"{Width}x{Height}";
+            }
         }
 
         private new ISpecification Spec { set { Spec(value); } }
@@ -23,24 +28,31 @@ namespace Mercury.BinPack.Tests
         {
             Spec = "When packing one item (#w x #h), "
                 .Arrange(() => new Packer<TypeToPack>(t => t.Width, t => t.Height))
-                .With(new { w = 1, h = 1 })
-                .With(new { w = 40, h = 30 })
-                .With(new { w = 20, h = 10 })
+                .With(new {w = 1, h = 1})
+                .With(new {w = 40, h = 30})
+                .With(new {w = 20, h = 10})
                 .Act((p, d) => p.Pack(new TypeToPack(d.w, d.h)))
                 .Assert("Total width is #w", (r, d) => Assert.AreEqual(d.w, r.TotalWidth))
                 .Assert("Total height is #h", (r, d) => Assert.AreEqual(d.h, r.TotalHeight));
 
             Spec = "When packing two items (#i1) (#i2), "
-              .Arrange(() => new Packer<TypeToPack>(t => t.Width, t => t.Height))
-              .With(new { i1 = new { w = 1, h = 1 }, i2 = new { w = 1, h = 1 }, expected = new { w = 2, h = 1 } })
-              .With(new { i1 = new { w = 1, h = 2 }, i2 = new { w = 1, h = 2 }, expected = new { w = 2, h = 2 } })
-              .With(new { i1 = new { w = 2, h = 1 }, i2 = new { w = 2, h = 1 }, expected = new { w = 2, h = 2 } })
-              .With(new { i1 = new { w = 3, h = 1 }, i2 = new { w = 2, h = 1 }, expected = new { w = 3, h = 2 } })
-              .With(new { i1 = new { w = 2, h = 1 }, i2 = new { w = 3, h = 1 }, expected = new { w = 3, h = 2 } })
-              .Act((p, d) => p.Pack(new TypeToPack(d.i1.w, d.i1.h), new TypeToPack(d.i2.w, d.i2.h)))
-              .Assert("Total width is #expected", (r, d) => Assert.AreEqual(d.expected.w, r.TotalWidth))
-              .Assert("Total height is #expected", (r, d) => Assert.AreEqual(d.expected.h, r.TotalHeight));
+                .Arrange(() => new Packer<TypeToPack>(t => t.Width, t => t.Height))
+                .With(new {i1 = new {w = 1, h = 1}, i2 = new {w = 1, h = 1}, expected = new {w = 2, h = 1}})
+                .With(new {i1 = new {w = 1, h = 2}, i2 = new {w = 1, h = 2}, expected = new {w = 2, h = 2}})
+                .With(new {i1 = new {w = 2, h = 1}, i2 = new {w = 2, h = 1}, expected = new {w = 2, h = 2}})
+                .With(new {i1 = new {w = 3, h = 1}, i2 = new {w = 2, h = 1}, expected = new {w = 3, h = 2}})
+                .With(new {i1 = new {w = 2, h = 1}, i2 = new {w = 3, h = 1}, expected = new {w = 3, h = 2}})
+                .Act((p, d) => p.Pack(new TypeToPack(d.i1.w, d.i1.h), new TypeToPack(d.i2.w, d.i2.h)))
+                .Assert("Total width is #expected", (r, d) => Assert.AreEqual(d.expected.w, r.TotalWidth))
+                .Assert("Total height is #expected", (r, d) => Assert.AreEqual(d.expected.h, r.TotalHeight));
 
+
+            Spec = "When packing three items #1, #2, #3, "
+                .Arrange(() => new Packer<TypeToPack>(t => t.Width, t => t.Height))
+                .With(new TypeToPack(1, 1), new TypeToPack(1, 1), new TypeToPack(1, 1), new Size(2, 2))
+                .Act((sut, a, b, c, expect) => sut.Pack(a, b, c))
+                .Assert("total width should be #4.Width",(r, expect) => Assert.AreEqual(expect.Width, r.TotalWidth))
+                .Assert("total height should be #4.Height", (r, expect) => Assert.AreEqual(expect.Height, r.TotalHeight));
         }
     }
 }
